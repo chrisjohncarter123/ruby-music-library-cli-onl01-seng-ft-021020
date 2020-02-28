d7a9281490c782d25295d0c59db129ebf61066fa
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-28 03:00:39 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,17 +24,10 @@ class MusicLibraryController
   end
   
   def list_songs
-    all_songs = []
-    Song.all.each do |song|
-      all_songs << song
-    end
     
-    all_songs = all_songs.sort_by!{ |song| song.name }
     
-    all_songs.each_with_index do |song, index|
-      result = "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-      
-      puts result
+    Song.all.sort_by!{ |song| song.name }.each_with_index do |song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
     
   end
