ec1d1b049dd737116026fc0b728ce70d4bc2edcb
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-28 03:08:52 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -53,6 +53,8 @@ class MusicLibraryController
     input = gets
     
     Artist.all.select{|artist| artist.name == input}.each_with_index do |artist, index|
+      
+      artist.
       puts "#{index + 1}. #{artist.name}"
     end
 
