5b37820c527b86498765c2385b85666ac6b12f64
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-28 02:45:21 +0000

    Automatically backed up by Learn

---
+++ lib/MusicLibraryController.rb
@@ -0,0 +1,19 @@
+class MusicLibraryController
+  
+  def initialize(path = "./db/mp3s")
+    music_importer = MusicImporter.new(path)
+    music_importer.import
+  end
+  
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    input = gets 
+    
+    while (input != "exit")
+      input = gets
+    end
+    
+    
+  end
+end
